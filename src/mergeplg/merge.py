6213d652d6b3969792e0ac0b250fd30e774f6ff1
EliaCovi <87362242+EliaCovi@users.noreply.github.com>
2025-01-13 11:58:36 +0100

    Multiplicative idw & update methods corrections (#30)
    - Added Multiplicative IDW with ratio range check (range check could later be removed, see #29)
    - fix bug with sorting of cml intersect_weights in update_ and update_block_ functions of Merge class.
    - Updated OpenRainER_case_highlevel notebook. 
    (copy-paste from PR description)

--- src/mergeplg/merge.py
+++ src/mergeplg/merge.py
@@ -155,8 +155,11 @@ class Merge:
                     # Add to existing x0
                     self.x0_cml = xr.concat([self.x0_cml, x0_cml_add], dim="cml_id")
 
-            # Sort x0_cml so it follows the same order as da_cml
+            # Sort x0_cml and self.intersect_weights
             self.x0_cml = self.x0_cml.sel(cml_id=da_cml.cml_id.data)
+            self.intersect_weights = self.intersect_weights.sel(
+                cml_id=da_cml.cml_id.data
+            )
 
         # If gauge data is present
         if da_gauge is not None:
@@ -293,8 +296,11 @@ class Merge:
                     # Add new x0 to self.x0_cml
                     self.x0_cml = xr.concat([self.x0_cml, x0_cml_add], dim="cml_id")
 
-            # Sort x0_cml so it follows the same order as da_cml
+            # Sort x0_cml and self.intersect_weights
             self.x0_cml = self.x0_cml.sel(cml_id=da_cml.cml_id.data)
+            self.intersect_weights = self.intersect_weights.sel(
+                cml_id=da_cml.cml_id.data
+            )
 
         # If gauge data is present
         if da_gauge is not None:
@@ -433,6 +439,113 @@ class Merge:
 
         # Return radar, observations and coordinates
         return observations_radar, observations_ground, x0
+
+
+class MergeMultiplicativeIDW(Merge):
+    """Merge CML and radar using an additive IDW (CML midpoint).
+
+    Merges the provided radar field in ds_rad to CML observations by
+    interpolating the difference between the CML and radar observations using
+    IDW.
+    """
+
+    def __init__(
+        self,
+        grid_location_radar="center",
+        min_obs=5,
+    ):
+        Merge.__init__(self, grid_location_radar, min_obs)
+
+    def update(self, da_rad, da_cml=None, da_gauge=None):
+        """Update weights and x0 geometry for CML and gauge
+
+        This function uses the midpoint of the CML as CML reference.
+        """
+        self.update_(da_rad, da_cml=da_cml, da_gauge=da_gauge)
+
+    def adjust(
+        self,
+        da_rad,
+        da_cml=None,
+        da_gauge=None,
+        p=2,
+        idw_method="radolan",
+        nnear=8,
+        max_distance=60000,
+    ):
+        """Adjust radar field for one time step.
+
+        Adjust radar field for one time step. The function assumes that the
+        weights are updated using the update class method.
+
+        Parameters
+        ----------
+        da_rad: xarray.DataArray
+            Gridded radar data. Must contain the lon and lat coordinates as
+            well as the projected coordinates xs and ys as a meshgrid.
+        da_cml: xarray.DataArray
+            CML observations. Must contain the lat/lon coordinates for the CML
+            (site_0_lon, site_0_lat, site_1_lon, site_1_lat) as well as the
+            projected midpoint coordinates (x, y).
+        da_gauge: xarray.DataArray
+            Gauge observations. Must contain the coordinates for the rain gauge
+            positions (lat, lon) as well as the projected coordinates (x, y).
+        p: float
+            IDW interpolation parameter
+        idw_method: str
+            by default "radolan"
+        nnear: int
+            number of neighbours to use for interpolation
+        max_distance: float
+            max distance allowed interpolation distance
+
+        Returns
+        -------
+        da_rad_out: xarray.DataArray
+            DataArray with the same structure as the ds_rad but with the CML
+            adjusted radar field.
+        """
+        # Update weights and x0 geometry for CML and gauge
+        self.update(da_rad, da_cml=da_cml, da_gauge=da_gauge)
+
+        # Evaluate radar at cml and gauge ground positions
+        rad, obs, x0 = self.radar_at_ground_(da_rad, da_cml=da_cml, da_gauge=da_gauge)
+
+        # Calculate radar-instrument difference if radar has observation
+        mask_zero = rad > 0
+        diff = np.full_like(obs, np.nan, dtype=np.float64)
+        diff[mask_zero] = obs[mask_zero] / rad[mask_zero]
+
+        # Get index of not-nan obs
+        keep = np.where((~np.isnan(diff)) & (diff < np.nanquantile(diff, 0.95)))[0]
+
+        # Check that that there is enough observations
+        if keep.size > self.min_obs_:
+            # get timestamp
+            time = da_rad.time.data[0]
+
+            # Remove radar time dimension
+            da_rad_t = da_rad.sel(time=time)
+
+            # do addtitive IDW merging
+            adjusted = merge_functions.merge_multiplicative_idw(
+                xr.where(da_rad_t > 0, da_rad_t, np.nan),  # function skips nan
+                diff[keep],
+                x0[keep, :],
+                p=p,
+                idw_method=idw_method,
+                nnear=nnear,
+                max_distance=max_distance,
+            )
+
+            # Replace nan with original radar data (so that da_rad nan is kept)
+            adjusted = xr.where(np.isnan(adjusted), da_rad_t, adjusted)
+
+            # Re-assign timestamp and return
+            return adjusted.assign_coords(time=time)
+
+        # Else return the unadjusted radar
+        return da_rad
 
 
 class MergeAdditiveIDW(Merge):
--- src/mergeplg/merge_functions.py
+++ src/mergeplg/merge_functions.py
@@ -92,6 +92,86 @@ def merge_additive_idw(
     return ds_rad_out.adjusted
 
 
+def merge_multiplicative_idw(
+    da_rad, cml_ratio, x0, p=2, idw_method="radolan", nnear=8, max_distance=60000
+):
+    """Merge CML and radar using an multiplicative approach and the CML midpoint.
+
+    Merges the CML and radar field by interpolating the ratio between
+    radar and CML using IDW from sklearn.
+
+    Parameters
+    ----------
+    da_rad: xarray.DataArray
+        Gridded radar data.
+    cml_ratio: numpy.array
+        Ratio between the CML and radar observations at the CML locations.
+    x0: numpy.array
+        Coordinates of CML midpoints given as [[cml_1_y, cml_1_x], ..
+        [cml_n_y, cml_n_x] using the same order as cml_ratio.
+    p: float
+        IDW interpolation parameter
+    idw_method: str
+        by default "radolan"
+    nnear: int
+        number of neighbours to use for interpolation
+    max_distance: float
+        max distance allowed interpolation distance
+
+    Returns
+    -------
+    da_rad_out: xarray.DataArray
+        DataArray with the same structure as the ds_rad but with the CML
+        adjusted radar field.
+
+    """
+    # Get radar grid as numpy arrays
+    xgrid, ygrid = da_rad.xs.data, da_rad.ys.data
+
+    # Create array for storing interpolated values
+    shift = np.zeros(xgrid.shape)
+
+    # Gridpoints to interpolate, skip cells with nan
+    mask = np.isnan(da_rad.data)
+
+    # Check that we have any data
+    if np.sum(~mask) > 0:
+        coord_pred = np.hstack(
+            [ygrid[~mask].reshape(-1, 1), xgrid[~mask].reshape(-1, 1)]
+        )
+
+        # IDW interpolator invdisttree
+        idw_interpolator = idw.Invdisttree(x0)
+        estimate = idw_interpolator(
+            q=coord_pred,
+            z=cml_ratio,
+            nnear=cml_ratio.size if cml_ratio.size <= nnear else nnear,
+            p=p,
+            idw_method=idw_method,
+            max_distance=max_distance,
+        )
+
+        shift[~mask] = estimate
+
+    # create xarray object similar to ds_rad
+    ds_rad_out = da_rad.rename("R").to_dataset().copy()
+
+    # Set areas with nan to zero
+    shift[np.isnan(shift)] = 0
+
+    # Do adjustment
+    adjusted = shift * ds_rad_out.R.data
+
+    # Set negative values to zero
+    adjusted = np.where(adjusted > 0, adjusted, 0)
+
+    # Store shift data
+    ds_rad_out["adjusted"] = (("y", "x"), adjusted)
+
+    # Return dataset with adjusted values
+    return ds_rad_out.adjusted
+
+
 def merge_additive_blockkriging(da_rad, cml_diff, x0, variogram, n_closest):
     """Merge CML and radar using an additive block kriging.
 
