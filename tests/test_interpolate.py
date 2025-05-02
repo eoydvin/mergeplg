022993be24c30ecd27345d614cb0ceb4328935a9
eoydvin <erlend.oydvin@gmail.com>
2025-05-02 10:59:23 +0200

    test kriging exact at points

--- tests/test_interpolate.py
+++ tests/test_interpolate.py
@@ -35,9 +35,9 @@ ds_gauges = xr.Dataset(
     coords={
         "id": ("id", ["gauge1", "gauge2", "gauge3"]),
         "time": ("time", np.arange(0, 4)),
-        "lon": ("id", [1, 0, 2]),
+        "lon": ("id", [-1, 0, 2]),
         "lat": ("id", [1, 1, 2]),
-        "x": ("id", [1, 0, 2]),
+        "x": ("id", [-1, 0, 2]),
         "y": ("id", [1, 1, 2]),
     },
 )
@@ -54,6 +54,162 @@ ds_rad = xr.Dataset(
         "y_grid": (("y", "x"), np.meshgrid([-1, 0, 1, 2], [-1, 0, 1, 2])[1]),
     },
 )
+
+
+def test_interpolator_OK_exact_at_points():
+    # Init data and interpolator, select CML and gauge data that do not overlap
+    ds_cmls_t1 = ds_cmls.isel(cml_id=[0, 1], time=1)
+    ds_gauges_t1 = ds_gauges.isel(id=[0, 1], time=1)
+    ds_grid = ds_rad.isel(time=0)
+
+    interpolator = interpolate.InterpolateOrdinaryKriging(
+        ds_grid=ds_rad,
+        ds_cmls=ds_cmls_t1,
+        ds_gauges=ds_gauges_t1,
+        min_observations=1,
+        variogram_parameters={"sill": 1, "range": 1, "nugget": 0},
+        full_line=False,
+    )
+
+    # Test that providing only RG works
+    interpolated = interpolator(
+        ds_grid,
+        da_gauges=ds_gauges_t1.R,
+    )
+    for gauge_id in ds_gauges_t1.id:
+        merge_r = interpolated.sel(
+            x=ds_gauges_t1.sel(id=gauge_id).x.data,
+            y=ds_gauges_t1.sel(id=gauge_id).y.data,
+        ).data
+        gauge_r = ds_gauges_t1.R.sel(id=gauge_id).data
+        np.testing.assert_almost_equal(
+            merge_r,
+            gauge_r,
+            decimal=6,
+        )
+
+    # Test that providing only CML works
+    interpolated = interpolator(
+        ds_grid,
+        da_cmls=ds_cmls_t1.R,
+    )
+    for cml_id in ds_cmls_t1.cml_id:
+        merge_r = interpolated.sel(
+            x=ds_cmls_t1.sel(cml_id=cml_id).x.data,
+            y=ds_cmls_t1.sel(cml_id=cml_id).y.data,
+        ).data
+        gauge_r = ds_cmls_t1.R.sel(cml_id=cml_id).data
+        np.testing.assert_almost_equal(
+            merge_r,
+            gauge_r,
+            decimal=6,
+        )
+
+    # Test that providing RG and CML works
+    interpolated = interpolator(
+        ds_grid,
+        da_cmls=ds_cmls_t1.R,
+        da_gauges=ds_gauges_t1.R,
+    )
+    for cml_id in ds_cmls_t1.cml_id:
+        merge_r = interpolated.sel(
+            x=ds_cmls_t1.sel(cml_id=cml_id).x.data,
+            y=ds_cmls_t1.sel(cml_id=cml_id).y.data,
+        ).data
+        gauge_r = ds_cmls_t1.R.sel(cml_id=cml_id).data
+        np.testing.assert_almost_equal(
+            merge_r,
+            gauge_r,
+            decimal=6,
+        )
+    for gauge_id in ds_gauges_t1.id:
+        merge_r = interpolated.sel(
+            x=ds_gauges_t1.sel(id=gauge_id).x.data,
+            y=ds_gauges_t1.sel(id=gauge_id).y.data,
+        ).data
+        gauge_r = ds_gauges_t1.R.sel(id=gauge_id).data
+        np.testing.assert_almost_equal(
+            merge_r,
+            gauge_r,
+            decimal=6,
+        )
+
+
+def test_interpolator_IDW_exact_at_points():
+    # Init data and interpolator, select CML and gauge that do not overlap
+    ds_cmls_t1 = ds_cmls.isel(cml_id=[0, 1], time=1)
+    ds_gauges_t1 = ds_gauges.isel(id=[0, 1], time=1)
+    ds_grid = ds_rad.isel(time=0)
+
+    interpolator = interpolate.InterpolateIDW(
+        ds_grid=ds_rad,
+        ds_cmls=ds_cmls_t1,
+        ds_gauges=ds_gauges_t1,
+        min_observations=1,
+    )
+
+    # Test that providing only RG works
+    interpolated = interpolator(
+        ds_grid,
+        da_gauges=ds_gauges_t1.R,
+    )
+    for gauge_id in ds_gauges_t1.id:
+        merge_r = interpolated.sel(
+            x=ds_gauges_t1.sel(id=gauge_id).x.data,
+            y=ds_gauges_t1.sel(id=gauge_id).y.data,
+        ).data
+        gauge_r = ds_gauges_t1.R.sel(id=gauge_id).data
+        np.testing.assert_almost_equal(
+            merge_r,
+            gauge_r,
+            decimal=6,
+        )
+
+    # Test that providing only CML works
+    interpolated = interpolator(
+        ds_grid,
+        da_cmls=ds_cmls_t1.R,
+    )
+    for cml_id in ds_cmls_t1.cml_id:
+        merge_r = interpolated.sel(
+            x=ds_cmls_t1.sel(cml_id=cml_id).x.data,
+            y=ds_cmls_t1.sel(cml_id=cml_id).y.data,
+        ).data
+        gauge_r = ds_cmls_t1.R.sel(cml_id=cml_id).data
+        np.testing.assert_almost_equal(
+            merge_r,
+            gauge_r,
+            decimal=6,
+        )
+
+    # Test that providing RG and CML works
+    interpolated = interpolator(
+        ds_grid,
+        da_cmls=ds_cmls_t1.R,
+        da_gauges=ds_gauges_t1.R,
+    )
+    for cml_id in ds_cmls_t1.cml_id:
+        merge_r = interpolated.sel(
+            x=ds_cmls_t1.sel(cml_id=cml_id).x.data,
+            y=ds_cmls_t1.sel(cml_id=cml_id).y.data,
+        ).data
+        gauge_r = ds_cmls_t1.R.sel(cml_id=cml_id).data
+        np.testing.assert_almost_equal(
+            merge_r,
+            gauge_r,
+            decimal=6,
+        )
+    for gauge_id in ds_gauges_t1.id:
+        merge_r = interpolated.sel(
+            x=ds_gauges_t1.sel(id=gauge_id).x.data,
+            y=ds_gauges_t1.sel(id=gauge_id).y.data,
+        ).data
+        gauge_r = ds_gauges_t1.R.sel(id=gauge_id).data
+        np.testing.assert_almost_equal(
+            merge_r,
+            gauge_r,
+            decimal=6,
+        )
 
 
 def test_interpolator_update():
